09d0d3d327250b8ad019bf66b026275ccda9ab4b
YADAVPRINCE304 <priyanshuyadav304@gmail.com>
2024-06-01 12:35:10 +0530

    dotenv

---
+++ src/db/database.js
@@ -0,0 +1,19 @@
+import { configDotenv } from "dotenv";
+
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+configDotenv();
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED",error);
+        process.exit(1)
+    }
+}
+
+export default connectDB 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 //require('dotenv').config({path: '/env'})
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 
 
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants";
-import connectDB from "./db/index.js";
+import connectDB from "./db/database.js";
 
-dotenv.config({
-    path:'./env'
-})
+// dotenv.config({
+//     path:'./env'
+// })
 
 
 
