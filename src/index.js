a99925697a6448fcb9b301bab96a6f414c3906e4
YADAVPRINCE304 <priyanshuyadav304@gmail.com>
2024-06-03 00:34:49 +0530

    util code extension

--- src/index.js
+++ src/index.js
@@ -13,8 +13,15 @@ import connectDB from "./db/database.js";
 
 
 connectDB()
-
-
+.then(()=>{
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`Server is running at port :$
+        {process.env.PORT}`);
+    })
+})
+.catch ((err)=>{
+    console.log("MONGO db connection failed", err);
+})
 
 /*import express from "express"
 const app = express()
